f86316e69635ba55cb232cb240a315c43c2da633
kadsfvcui <1509797189@qq.com>
2024-09-22 21:09:17 +0800

    Delete

--- aaa.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char a[10];
-    scanf("%s",&a[10]);
-    printf("%s\n",a);
-    return 0;
-}
--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-	char str[10]={'h','e','l','l','o','\0','w','o'};
-    printf("%s",str);
-	return 0;
-}
